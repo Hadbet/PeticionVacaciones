038478c519a4a25bc225628434c23fdead4f8a7f
hadbet <hadbet.altamirano@grammer.com>
2025-03-27 11:56:07 -0600

    Inicio del proyecto

--- dao/daoSupervisor.php
+++ dao/daoSupervisor.php
@@ -4,18 +4,29 @@ include_once('db/db_RH.php');
 
 $APU = $_GET['APU'];
 
-Contador($APU);
+try {
+    Contador($APU);
+} catch (Exception $e) {
+    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+}
 
 function Contador($APU)
 {
     $con = new LocalConector();
     $conex = $con->conectar();
 
+    if (!$conex) {
+        throw new Exception('No se pudo conectar: ' . mysqli_error($conex));
+    }
+
     $datos = mysqli_query($conex, "SELECT `Supervisor` FROM `Encargados` WHERE `APU` LIKE '$APU' GROUP BY `Supervisor`;");
+
+    if (!$datos) {
+        throw new Exception('Error en la consulta: ' . mysqli_error($conex));
+    }
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
 }
-
 
 ?>
