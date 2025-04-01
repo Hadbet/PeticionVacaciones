b2c4f95043b5d83c05d6b0cfa8b93633155ed039
hadbet <hadbet.altamirano@grammer.com>
2025-04-01 11:56:41 -0600

    Inicio del proyecto

---
+++ dao/daoConfiguraciones.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once('db/db_Management.php');
+
+Contador();
+
+function Contador()
+{
+    $con = new LocalConector();
+    $conex = $con->conectar();
+
+    $datos = mysqli_query($conex, "SELECT * FROM `ConfiguracionVacaciones`");
+
+    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+    echo json_encode(array("data" => $resultado));
+}
+
+
+?>
